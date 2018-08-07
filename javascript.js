3369a5e9fa606d9847281ac29079e0f2df4e6a96
Athiya Deviyani <40429401+athiyadeviyani@users.noreply.github.com>
2018-08-07 19:09:21 +0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -238,8 +238,3 @@ function share() {
     var url = "www.github.com/athiyadeviyani";
     window.open('http://twitter.com/share?url='+encodeURIComponent(url)+'&text='+encodeURIComponent(text), '', 'left=0,top=0,width=550,height=450,personalbar=0,toolbar=0,scrollbars=0,resizable=0');
 }
-
-var siteWidth = 1280;
-var scale = screen.width /siteWidth
-
-document.querySelector('meta[name="viewport"]').setAttribute('content', 'width='+siteWidth+', initial-scale='+scale+'')
